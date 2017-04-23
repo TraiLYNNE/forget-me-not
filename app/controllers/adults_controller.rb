bd78e1a467e3e8d0cebcb40962b4a21d2a964517
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 19:38:34 +0000

    setting up controller action to render form for new adult

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -1,3 +1,9 @@
 class AdultsController < ApplicationController
-
+  get '/adults/new' do
+    if logged_in?
+      erb :'adults/create_adult'
+    else
+      redirect to '/login'
+    end
+  end
 end
