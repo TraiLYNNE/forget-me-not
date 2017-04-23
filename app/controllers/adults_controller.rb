b3cad394c9bad2d90ab8cfa3cfc00da5a1d56378
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 22:47:46 +0000

    adding show view for individual child

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -52,21 +52,4 @@ class AdultsController < ApplicationController
       redirect to "/adults/#{adult.slug}"
     end
   end
-
-  patch '/adults/:id' do
-    adult = Adult.find_by_id(params[:id])
-
-    if params["adult"]["first_name"] == "" || params["adult"]["last_name"] == "" || params["adult"]["birth_date"] == ""
-      redirect to "/adults/#{adult.id}/edit"
-    else
-      adult.update(params[:adult])
-
-      if params["child"]["first_name"] != "" || params["child"]["last_name"] != "" || params["child"]["birth_date"] != ""
-        adult.children << child = Child.create(params[:child])
-        current_user.children << child
-      end
-
-      redirect to "/adults/#{adult.slug}"
-    end
-  end
 end
