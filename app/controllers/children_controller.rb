1d0d82ad8279373eaad5cce24e7479ce90b287f0
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 22:36:45 +0000

    adding index controller action for children

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,3 +1,10 @@
 class ChildrenController < ApplicationController
-
+  get '/children' do
+    if logged_in?
+      @adults = current_user.children.sort_by{|a| a.last_name}
+      erb :'children/index'
+    else
+      redirect to '/login'
+    end
+  end
 end
