9a142470d2fd713fa289aeac0acc42306248e813
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 20:29:51 +0000

    adding slugifiable module to adult and child models

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -1,4 +1,8 @@
 class AdultsController < ApplicationController
+  get '/adults/:slug' do
+
+  end
+
   get '/adults/new' do
     if logged_in?
       @children = current_user.children
@@ -9,6 +13,16 @@ class AdultsController < ApplicationController
   end
 
   post '/adults' do
+    adult = Adult.create(params[:adult])
 
+    adult.children << child = Child.create(params[:child]) if !params["child"]["first_name"].empty? || !params["child"]["last_name"].empty?
+
+    adult.user_id = current_user.id
+    adult.save
+
+    child.user_id = current_user.id
+    child.save
+
+    redirect to '/users/home'
   end
 end
