ed00dd4f669090c3ca574867978e04259f8df31f
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 19:03:17 +0000

    set up post controller action for user login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   get '/login' do
     if !logged_in?
       erb :'/users/login'
+    else
+      redirect to '/'
     end
   end
 
@@ -19,6 +21,16 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
 
       redirect to '/'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/"
+    else
+      redirect to '/signup'
     end
   end
 end
