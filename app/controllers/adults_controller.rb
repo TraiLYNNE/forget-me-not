8cb6a5d169191569aa4992e4dd880ffec93506c4
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 19:59:17 +0000

    setting up new adult form

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -1,9 +1,14 @@
 class AdultsController < ApplicationController
   get '/adults/new' do
     if logged_in?
+      @children = current_user.children
       erb :'adults/create_adult'
     else
       redirect to '/login'
     end
+  end
+
+  post '/adults' do
+
   end
 end
