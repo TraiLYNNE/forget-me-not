8f3c3f8778c6140bcbbbe0cabf7bda0b8e8fa63b
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 18:42:36 +0000

    adding helper methods and post controller action for signup form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   get '/signup' do
-    erb :'users/create_user'
+    if !logged_in?
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      user = User.create(username:params[:username], email: params[:email], password:params[:password])
+      session[:user_id] = user.id
+    end
   end
 end
