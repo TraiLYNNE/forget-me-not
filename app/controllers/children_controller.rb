a676d78ab056b8b3d5e0e78161244389cf518f9f
Trai Compton <tlcompton2546@outlook.com>
2017-04-23 22:42:24 +0000

    adding post controller action for child creation

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -7,4 +7,30 @@ class ChildrenController < ApplicationController
       redirect to '/login'
     end
   end
+
+  get '/children/new' do
+    if logged_in?
+      @adults = current_user.adults
+      erb :'adults/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/children' do
+    if params["child"]["first_name"] == "" || params["child"]["last_name"] == "" || params["child"]["birth_date"] == ""
+      redirect to '/children/new'
+    else
+      child = Child.create(params[:adult])
+      current_user.children << child
+
+      if params["adult"]["first_name"] != "" || params["adult"]["last_name"] != "" || params["adult"]["birth_date"] != ""
+        child.adults << adult = Child.create(params[:child])
+        current_user.adults << adult
+      end
+
+      redirect to "/children/#{child.slug}"
+    end
+  end
+
 end
